78025e67e1378a9b9bbc79940ab0f6801c05fb33
Nick <150211508+n1cksway@users.noreply.github.com>
2024-02-12 04:20:24 +0000

    nicksway-trafficlight

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,71 @@
-import React from "react";
-
+import React, { useState } from "react";
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	const [ selectedColor, setselectedColor ] = useState("red");
+	const [divCount, setDivCount] = useState(0);
+
+	const addNewDiv = () => {
+	  setDivCount(divCount + 1);
+	};
+	const handleButtonClick = () => {
+		
+		const colors = ["red", "yellow", "green","extra"];
+		const currentIndex = colors.indexOf(selectedColor);
+		const nextIndex = (currentIndex + 1) % colors.length;
+		setselectedColor(colors[nextIndex]);
+
+	};	
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="">
+			<div className="traffic-light">
+				<div 
+					onClick={() => setselectedColor("red")}
+					className={	
+						"light red" + (selectedColor ==="red" ? " glow-red" : "")}>
+
+				</div>
+				<div
+					onClick={() => setselectedColor("yellow")} 
+					className={
+						"light yellow" + (selectedColor ==="yellow" ? " glow-yellow" : "")
+					}>		
+				</div>
+			
+				<div
+					onClick={() => setselectedColor("green")}
+					className={
+						"light green" + (selectedColor ==="green" ? " glow-green" : "")}>		
+				</div>
+				<div>
+					{Array.from({ length: divCount }, (_, index) => (
+					<div  onClick={() => setselectedColor("extra")}
+					className={
+						"light extra" + (selectedColor ==="extra" ? " extra-glow" : "")}>	
+					</div>
+				))}
+			</div>
+
+			</div>
+			
+			
+			<div className="">
+				<button 
+				onClick={handleButtonClick}
+				className={
+					"btn btn-danger"  }>
+						click me</button>
+				<button 
+						onClick={() => addNewDiv(setselectedColor("extra"))}
+						className={"btn btn-success"}>
+							extra light</button>
+	
+			</div>
+			
 		</div>
+		
 	);
 };
 
